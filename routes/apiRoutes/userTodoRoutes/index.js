8c4646dcf1579d8c5b5aedd7e50a37cd3c26b489
emmanueljucaban <emmanueljucaban@gmail.com>
2020-05-05 22:21:11 -0700

    Push boilerplate

--- routes/apiRoutes/userTodoRoutes/index.js
+++ routes/apiRoutes/userTodoRoutes/index.js
@@ -5,8 +5,8 @@ const { requireAuth } = require('../../../middlewares/authMiddlewares');
 router.route('/')
   .get(requireAuth, userTodosController.getUserTodos);
 
-
 router.route('/:todoId')
   .delete(requireAuth, userTodosController.deleteUserTodoById)
   .put(requireAuth, userTodosController.updateTodoById);
+
 module.exports = router;
